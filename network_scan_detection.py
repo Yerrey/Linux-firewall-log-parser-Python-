34ce986f4f060cbeeb33c72f67fe0c51aec84614
Rey Maldonado <rey.maldonado357@gmail.com>
2026-02-12 22:04:56 -0600

    Add TCP scan classification logic

--- network_scan_detection.py
+++ network_scan_detection.py
@@ -2,6 +2,7 @@
 
 import re
 
+#test_file is a small snippet of the actual log. 
 test_file = "/var/log/iptables.log.1"
 
 #compiled regex patterns for syslog file 
@@ -12,6 +13,7 @@ PROTO = re.compile(r'\bPROTO=(?P<proto>TCP|UCP|ICMP)')
 SPT = re.compile(r'\bSPT=(?P<src_port>\d+)')
 DPT = re.compile(r'\bDPT=(?P<dst_port>\d+)')
 
+#patterns to loop over 
 patterns = [SRC, DST, PROTO, SPT, DPT]
 
 
@@ -20,11 +22,40 @@ with open(test_file, 'r') as f:
         extracted = {}
 
         for pattern in patterns:
-            key = pattern.search(line)
-            if key:
-                extracted.update(key.groupdict())
+            key_field = pattern.search(line)
+            if key_field:
+                extracted.update(key_field.groupdict())
+
+            FLAGS = {
+                "SYN": bool(re.search(r'\bSYN\b', line)),
+                "FIN": bool(re.search(r'\bFIN\b', line)),
+                "ACK": bool(re.search(r'\bACK\b', line)),
+                "PSH": bool(re.search(r'\bPSH\b', line)),
+                "URG": bool(re.search(r'\bURG\b', line)),
+                "RST": bool(re.search(r'\bRST\b', line)),
+            }
 
         if extracted:
-            print("SCAN DETECTED: ")
-            print(extracted)
-
+            if FLAGS["SYN"] and not FLAGS["ACK"]:
+                scan = "SYN SCAN"
+                print(f"{scan} Detected: ")
+                print(extracted)
+            elif FLAGS["FIN"] and FLAGS["PSH"] and FLAGS["URG"]:
+                scan = "XMAS SCAN"
+                print(f"{scan} Detected: ")
+                print(extracted)
+            elif FLAGS["FIN"] and not FLAGS["SYN"]:
+                scan = "FIN SCAN" 
+                print(f"{scan} Detected: ")
+                print(extracted)
+            elif FLAGS["ACK"] and not FLAGS["SYN"]:
+                scan = "ACK SCAN"
+                print(f"{scan} Detected: ")
+                print(extracted)
+            elif not any(FLAGS.values()):
+                scan = "NULL SCAN"
+                print(f"{scan} Detected: ")
+            else:
+                scan = "UNKNOWN SCAN"
+                print(f"{scan} Detected: ")
+                print(extracted)
