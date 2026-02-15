ffd01696229688491216585ebabddd0b46a8859d
Rey Maldonado <rey.maldonado357@gmail.com>
2026-02-14 20:38:57 -0600

    JSON output for readability

--- network_scan_detection.py
+++ network_scan_detection.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import re
+import json 
+
 
 #test_file is a small snippet of the actual log. 
 test_file = "/var/log/iptables.log.1"
@@ -26,36 +28,40 @@ with open(test_file, 'r') as f:
             if key_field:
                 extracted.update(key_field.groupdict())
 
-            FLAGS = {
+        FLAGS = {
                 "SYN": bool(re.search(r'\bSYN\b', line)),
                 "FIN": bool(re.search(r'\bFIN\b', line)),
                 "ACK": bool(re.search(r'\bACK\b', line)),
                 "PSH": bool(re.search(r'\bPSH\b', line)),
                 "URG": bool(re.search(r'\bURG\b', line)),
                 "RST": bool(re.search(r'\bRST\b', line)),
-            }
+        }
 
         if extracted:
             if FLAGS["SYN"] and not FLAGS["ACK"]:
                 scan = "SYN SCAN"
-                print(f"{scan} Detected: ")
-                print(extracted)
+                print(f"{scan} Detected: \n")
+                print(json.dumps(extracted, indent = 4))
+                print()
             elif FLAGS["FIN"] and FLAGS["PSH"] and FLAGS["URG"]:
                 scan = "XMAS SCAN"
-                print(f"{scan} Detected: ")
-                print(extracted)
+                print(f"{scan} Detected: \n")
+                print(json.dumps(extracted, indent = 4))
+                print()
             elif FLAGS["FIN"] and not FLAGS["SYN"]:
                 scan = "FIN SCAN" 
-                print(f"{scan} Detected: ")
-                print(extracted)
+                print(f"{scan} Detected: \n")
+                print(json.dumps(extracted, indent = 4))
+                print()
             elif FLAGS["ACK"] and not FLAGS["SYN"]:
                 scan = "ACK SCAN"
-                print(f"{scan} Detected: ")
-                print(extracted)
+                print(f"{scan} Detected: \n")
+                print(json.dumps(extracted, indent = 4))
+                print()
             elif not any(FLAGS.values()):
                 scan = "NULL SCAN"
                 print(f"{scan} Detected: ")
+                print(json.dumps(extracted, indent = 4))
+                print()
             else:
-                scan = "UNKNOWN SCAN"
-                print(f"{scan} Detected: ")
-                print(extracted)
+                continue
