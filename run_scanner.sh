fb242694d874c163e01749f2ff49370c1dfdfb1a
Rey Maldonado <rey.maldonado357@gmail.com>
2026-03-12 19:19:10 -0500

    remove local wrapper script

--- run_scanner.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/usr/bin/python3 /home/reyma/python_projects/ids_project/linux-firewall-log-parser/network_scan_detection.py >> /home/reyma/python_projects/ids_project/linux-firewall-log-parser/test_scans.jsonl
