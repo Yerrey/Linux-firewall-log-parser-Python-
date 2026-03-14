48159f4a93e55efd982cabb8778d84c97c6df6b3
Rey Maldonado <rey.maldonado357@gmail.com>
2026-03-13 20:44:23 -0500

    added offset to avoid reading the whole file over and over

--- network_scan_detection.py
+++ network_scan_detection.py
@@ -13,7 +13,8 @@ load_dotenv()
 #file paths 
 file = "/var/log/iptables.log"
 output_file = os.getenv("OUTPUT_FILE")
-time = datetime.datetime.now().isoformat()
+offset_file = os.getenv("OFFSET_FILE")
+
 
 
  
@@ -27,6 +28,19 @@ DPT = re.compile(r'\bDPT=(?P<dst_port>\d+)')
 
 patterns = [SRC, DST, PROTO, SPT, DPT]
 
+def load_offset(offset_file):
+    if os.path.exists(offset_file):
+        with open(offset_file, 'r') as f:
+            content = f.read().strip()
+            if not content:
+                return 0, None
+            data = json.loads(content)
+            return data.get("offset", 0), data.get("inode", None)
+    return 0, None
+
+def save_offset(offset_file, offset, inode):
+    with open(offset_file, "w") as f:
+        json.dump({"offset": offset, "inode": inode}, f)
 
 
 def log_scan(scan_type, details, output_file):
@@ -37,10 +51,20 @@ def log_scan(scan_type, details, output_file):
             "scan_type": scan_type,
             "details": details
         }
-        out.write(json.dumps(scan_data) + '\n')
+        out.write(json.dumps(scan_data, indent = 4) + '\n')
 
+
+
+saved_offset, saved_inode = load_offset(offset_file)
+current_inode = os.stat(file).st_ino
+
+if saved_inode is not None and current_inode != saved_inode:
+    saved_offset = 0
 
 with open(file, 'r') as f:
+    f.seek(saved_offset)
+
+    
     for line in f:
         if "NETWORK SCAN DETECTED" not in line:
             continue
@@ -62,30 +86,24 @@ with open(file, 'r') as f:
         }
 
         if extracted:
+
+            scan = None
+            
             if FLAGS["SYN"] and not FLAGS["ACK"]:
                 scan = "SYN SCAN"
-                log_scan(scan, extracted, output_file)
-                print()
-                
             elif FLAGS["FIN"] and FLAGS["PSH"] and FLAGS["URG"]:
                 scan = "XMAS SCAN"
-                log_scan(scan, extracted, output_file)
-                print()
-                
             elif FLAGS["FIN"] and not FLAGS["SYN"]:
                 scan = "FIN SCAN"
-                log_scan(scan, extracted, output_file)
-                print()
-                
             elif FLAGS["ACK"] and not FLAGS["SYN"]:
                 scan = "ACK SCAN"
-                log_scan(scan, extracted, output_file)
-                print()
-                
             elif not any(FLAGS.values()):
                 scan = "NULL SCAN"
-                log_scan(scan , extracted, output_file)
-                print()
-            else:
-                continue
+
+            if scan:
+                log_scan(scan,extracted,output_file)
+
+    save_offset(offset_file, f.tell(), current_inode)
+                
+                        
                 
